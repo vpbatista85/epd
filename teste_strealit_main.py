88ce9defc975ab062d1eaccce92b6f67a2d5599c
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-16 12:13:38 -0300

    ignoring seaborn import to avoind error

--- teste_strealit_main.py
+++ teste_strealit_main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 
 if 'l_prod' not in st.session_state:
