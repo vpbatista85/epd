4ecb53f121a47ae2db2e1d7870c03cd56127ff9e
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-17 16:04:15 -0300

    several changes on functions for non personal recommendations

--- teste_strealit_main.py
+++ teste_strealit_main.py
@@ -4,12 +4,8 @@ import numpy as np
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 
+def f_escolha(df):
 
-if 'l_prod' not in st.session_state:
-    st.session_state.l_prod = []
-
-def f_escolha(df,l_prod):
-    
     st.title("Bem vindo!")
         #Seleção da loja 
 
@@ -19,11 +15,15 @@ def f_escolha(df,l_prod):
     ##Seleção dos campos referente ao produto:
     st.write('Selecione o produto para o carrinho:')
     df_loja=df[df['loja_compra']==store]
+    df_loja_recnp=df_loja.copy()
+    df_loja_recnp['produto_f']=df_loja_recnp['produto']+" "+df_loja_recnp['prodcomplemento']
+
     #Seleção da categoria do produto
     cat = st.selectbox(
         'Selecione a categoria:',
         df_loja.categoria.unique())
     df_cat=df_loja[df_loja['categoria']==cat]
+
     #Seleção do tipo do produto
     tipo = st.selectbox(
         'Selecione o tipo:',
@@ -35,9 +35,20 @@ def f_escolha(df,l_prod):
         df_tipo.produto.unique())
     df_prod=df_tipo[df_tipo['produto']==product]
     #Seleção do complemento
+
+    if df_prod.prodcomplemento.isin([""]).count()>=1 and len(df_prod.prodcomplemento.unique())==1:
+            p_dis=True
+            p_vis="collapsed"
+    else:
+        p_dis=False
+        p_vis="visible"
+
     complement=st.selectbox(
         'Selecione o complemento:',
-        df_prod.prodcomplemento.unique())
+        df_prod.prodcomplemento.unique(),
+        disabled=p_dis,
+        label_visibility=p_vis)
+
     df_compl=df_prod[df_prod['prodcomplemento']==complement]
 
     prodf=product+str(complement)
@@ -46,9 +57,10 @@ def f_escolha(df,l_prod):
     if st.button('Add carrinho'):
             st.write(prodf,"adicionado ao carrinho")
             st.session_state.l_prod.append(prodf)
-          
+
+    return df_loja_recnp
             
-def f_carrinho(l_prod):
+def f_carrinho():
         import streamlit as st
         placeholder = st.empty()
         placeholder.text("Carrinho:")
@@ -57,8 +69,46 @@ def f_carrinho(l_prod):
             st.write('Carrinho:')
             for i in st.session_state.l_prod:
                 st.write(i)
-            
 
+
+def rnp_apr():
+    #Recomendação não personalizada utilizando o algoritimo apriori.
+    return
+
+def rec_top_n(ratings:pd.DataFrame, n:int) -> pd.DataFrame:
+    #Recomendação não personalizada por n produtos mais consumidos.
+    recommendations = (
+        ratings
+        .groupby('produto_f')
+        .count()['cliente_nome']
+        .reset_index()
+        .rename({'cliente_nome': 'score'}, axis=1)
+        .sort_values(by='score', ascending=False)
+    )
+
+    return recommendations.head(n)           
+
+def r_np(df_loja_recnp,l_prod): 
+    if len(l_prod)==0:
+        placeholder1 = st.empty() 
+    else:
+        tab1, tab2, = st.tabs(["Apriori", "Top N"])
+        with tab1:
+            rec_np=rec_top_n(df_loja_recnp,n=5)
+            placeholder1 = st.empty()
+            placeholder1.text("Adicione ao carrinho os produtos mais vendidos:")
+            with placeholder1.container():
+                    st.write("Adicione ao carrinho os produtos mais vendidos:")
+                    for i in rec_np.produto_f:
+                        st.write(i)
+        with tab2:
+            rec_np=rec_top_n(df_loja_recnp,n=5)
+            placeholder1 = st.empty()
+            placeholder1.text("Adicione ao carrinho os produtos mais vendidos:")
+            with placeholder1.container():
+                    st.write("Adicione ao carrinho os produtos mais vendidos:")
+                    for i in rec_np.produto_f:
+                        st.write(i)            
 
 def main():
     #lista de produtos no carrinho
@@ -66,13 +116,19 @@ def main():
     df_server= pd.read_csv(r"https://github.com/vpbatista85/epd/blob/main/output.csv?raw=true", encoding = 'utf-8')
     df=df_server.copy()
     df.fillna("",inplace=True)
-    f_escolha(df,l_prod)
-    f_carrinho(l_prod)
+    df_loja_recnp=f_escolha(df)
+    f_carrinho()
+    r_np(df_loja_recnp,st.session_state.l_prod)
+
+
+
 
 #inicio main:
-l_prod=[]
+
+if 'l_prod' not in st.session_state:
+    st.session_state.l_prod = []
 if __name__ == "__main__":
-    main()
+     main()
 
 
 
