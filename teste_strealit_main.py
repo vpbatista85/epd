37fcd563aac9401e1700244863466cdbdadd6700
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-21 00:50:22 -0300

    another corrections on buttons del and remove

--- teste_strealit_main.py
+++ teste_strealit_main.py
@@ -84,14 +84,19 @@ def f_carrinho():
                 st.write(i)
 
         if st.button('Del item',disabled=state):
-                st.session_state.l_prod.pop()
-                st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
-                placeholder.empty()  
-                placeholder.text("Carrinho:")
-                with placeholder.container():
-                    st.write('Carrinho:')
-                    for i in st.session_state.l_prod:
-                         st.write(i)
+                if len(st.session_state.l_prod)==1:
+                    st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
+                    st.session_state.l_prod=[]
+                    placeholder.empty()
+                else:    
+                    st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
+                    st.session_state.l_prod.pop()
+                    placeholder.empty()  
+                    placeholder.text("Carrinho:")
+                    with placeholder.container():
+                        st.write('Carrinho:')
+                        for i in st.session_state.l_prod:
+                            st.write(i)
 
         if st.button('Del carrinho',disabled=state):
                 st.session_state.l_prod=[]
