ad4484c715858efceaae8cb25a161baf286523fd
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-26 11:05:26 -0300

    another features on page change

--- teste_strealit_main.py
+++ teste_strealit_main.py
@@ -24,8 +24,33 @@ from streamlit.source_util import (
     get_pages,
     _on_pages_changed
 )
+from streamlit.components.v1 import html
 
-
+# create navigation
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
 
 
 def delete_page(main_script_path_str, page_name):
@@ -88,8 +113,8 @@ def f_escolha(df):
         st.session_state.store=store
         st.session_state.user=user
         add_page('teste_strealit_main.py', 'shop')
-        pages = get_pages('teste_strealit_main.py')
-        pages[-1]
+        nav_page('teste_strealit_main.py')
+
 
     ##Seleção dos campos referente ao produto:
     st.write('Selecione o produto para o carrinho:')
