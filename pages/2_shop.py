72308ea2ef9ec42aee335dcdab02e1ca28c4e7c5
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-26 11:45:01 -0300

    another features on page change

--- pages/2_shop.py
+++ pages/2_shop.py
@@ -2,6 +2,62 @@ import streamlit as st
 import pandas as pd
 import teste_strealit_main
 
+# create navigation
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+
+
+def delete_page(main_script_path_str, page_name):
+
+    current_pages = get_pages(main_script_path_str)
+
+    for key, value in current_pages.items():
+        if value['page_name'] == page_name:
+            del current_pages[key]
+            break
+        else:
+            pass
+    _on_pages_changed.send()
+
+def add_page(main_script_path_str, page_name):
+    
+    pages = get_pages(main_script_path_str)
+    main_script_path = Path(main_script_path_str)
+    pages_dir = main_script_path.parent / "pages"
+    script_path = [f for f in pages_dir.glob("*.py") if f.name.find(page_name) != -1][0]
+    script_path_str = str(script_path.resolve())
+    pi, pn = page_icon_and_name(script_path)
+    psh = calc_md5(script_path_str)
+    pages[psh] = {
+        "page_script_hash": psh,
+        "page_name": pn,
+        "icon": pi,
+        "script_path": script_path_str,
+    }
+    _on_pages_changed.send()
+
 #store=st.session_state.store
 #user=st.session_state.user
 #df=st.session_state.df
@@ -55,3 +111,5 @@ else:
 if st.button('Add carrinho'):
         st.write(prodf,"adicionado ao carrinho.")
         st.session_state.l_prod.append(prodf)
+        add_page('teste_strealit_main.py', 'cart')
+        nav_page('cart')
