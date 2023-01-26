f66a49c94b29e614776d84360af620bed7af1c2a
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-26 12:12:30 -0300

    another features on page change

--- pages/3_cart.py
+++ pages/3_cart.py
@@ -14,24 +14,25 @@ def f_carrinho():
 
 if len(st.session_state.l_prod)==0:
         state=True
-    else:
+else:
         state=False
-    if st.button('Del item',disabled=state):
-        if len(st.session_state.l_prod)==1:
-            st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
-            st.session_state.l_prod=[]
-            placeholder.empty()
-        else:    
-            st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
-            st.session_state.l_prod.pop()
-            placeholder.empty()  
-            placeholder.text("Carrinho:")
-            with placeholder.container():
-                st.write('Carrinho:')
-                for i in st.session_state.l_prod[0:-2]:
-                    st.write(i)
 
-    if st.button('Del carrinho',disabled=state):
+if st.button('Del item',disabled=state):
+    if len(st.session_state.l_prod)==1:
+        st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
+        st.session_state.l_prod=[]
+        placeholder.empty()
+    else:    
+        st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
+        st.session_state.l_prod.pop()
+        placeholder.empty()  
+        placeholder.text("Carrinho:")
+        with placeholder.container():
+            st.write('Carrinho:')
+            for i in st.session_state.l_prod[0:-2]:
+                st.write(i)
+
+if st.button('Del carrinho',disabled=state):
         st.session_state.l_prod=[]
         st.write (f"Carrinho limpo.") 
         placeholder.empty()
