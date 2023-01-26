82da996ed86681bb9b09830905163aeb9aee815e
vpbatista85 <vitor_pbatista@outlook.com>
2023-01-26 12:29:18 -0300

    another features on page change

--- pages/3_cart.py
+++ pages/3_cart.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import sklearn
+import mlxtend
+import os
+from collections import Counter
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+
+from funk_svd import SVD
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+from sklearn.pipeline import Pipeline
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 def f_carrinho():
         import streamlit as st
@@ -10,6 +28,213 @@ def f_carrinho():
             st.write('Carrinho:')
             for i in st.session_state.l_prod:
                 st.write(i)
+def rnp_apr(dfs:pd.DataFrame,l_prod):
+    #Recomendação não personalizada utilizando o algoritimo apriori.
+    ##agrupando os pedidos
+    df_l=dfs[['cod_pedido','produto_f']].groupby('cod_pedido').agg({'produto_f': lambda x : ','.join(set(x))})
+    df_l.rename(columns={'produto_f':'itens'},inplace=True)
+    df_l.reset_index(inplace=True)
+    df_l['itens']=df_l.itens.str.split(pat=',')
+    df_l.head()
+    ##aplicando as funções do mlxtend:
+    encoder=TransactionEncoder()
+    te_array=encoder.fit(list(df_l.itens)).transform(list(df_l.itens))
+    dft=pd.DataFrame(data=te_array,columns=encoder.columns_)
+    frequent_items=apriori(dft,min_support=0.01,use_colnames=True)
+    frequent_items['length'] = frequent_items['itemsets'].apply(lambda x: len(x))
+    rules=association_rules(frequent_items, metric='lift',min_threshold=1.0)
+    rules.sort_values(by='lift',ascending=False)
+    rules.antecedents=rules.antecedents.astype('string')
+    rules.consequents=rules.consequents.astype('string')
+    rules.antecedents=rules.antecedents.str.strip('frozenset({})')
+    rules.consequents=rules.consequents.str.strip('frozenset({})')
+    #recomendação
+    recommendations=pd.DataFrame(columns=rules.columns)
+    for i in l_prod:
+        recommendations=pd.concat([recommendations,rules[rules.antecedents.str.contains(i, regex=False)]],ignore_index=True)
+    for i in l_prod:
+        recommendations=recommendations[recommendations.consequents.str.contains(i, regex=False)==False]
+    recommendations.consequents.drop_duplicates(inplace=True)
+   
+    return recommendations
+
+def rnp_top_n(ratings:pd.DataFrame, n:int, l_prod:list) -> pd.DataFrame:
+    #Recomendação não personalizada por n produtos mais consumidos.
+    recommendations = (
+        ratings
+        .groupby('produto_f')
+        .count()['cliente_nome']
+        .reset_index()
+        .rename({'cliente_nome': 'score'}, axis=1)
+        .sort_values(by='score', ascending=False)
+    )
+    for i in l_prod:
+      recommendations=recommendations[recommendations.produto_f.str.contains(i, regex=False)==False]
+    return recommendations.head(n)  
+
+def rnp_cb(df:pd.DataFrame,l_prod:list)-> pd.DataFrame:
+    #preparando o dataframe para aplicação do algoritimo:
+    dfl=df.reset_index()
+    dfl['produto_full']=dfl['categoria']+" "+dfl['tipo_categoria']+" "+dfl['produto']+" "+dfl['prodcomplemento']
+    dfl['produto_f']=dfl['produto']+" "+dfl['prodcomplemento']
+    BASE_FEATURES=['index','produto_f','produto_full']
+
+    #definindo os vetores maximamente esparços:
+    df_gd=pd.get_dummies(dfl[['categoria','tipo_categoria','produto','prodcomplemento']])
+
+    #unindo ao df da loja (dfl):
+    df_l=dfl[BASE_FEATURES].merge(df_gd,left_index=True,right_index=True)
+
+    #agrupando por pelo nome completo  para encontrar a matriz maximamente esparça dos items:
+    df_ll=df_l.groupby('produto_full').max()
+    df_ll.set_index('index',inplace=True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    df_ll.index.name = 'id'
+    df_ll.index=df_ll.index.astype(str)
+    df_ll.columns=df_ll.columns.astype(str)
+    df_train=df_ll.iloc[:,1:]
+    pipeline = Pipeline([('scaler', MinMaxScaler())])
+    pipeline.fit(df_train)
+
+    #gerando a representação vetorial:
+    df_vectors = pd.DataFrame(pipeline.transform(df_train))
+    df_vectors.columns = df_train.columns
+    df_vectors.index = df_train.index
+    df_vectors.index.name = 'id'
+
+    #Calculando a matriz de similaridade item-item:
+    similarity_matrix = pd.DataFrame(cosine_similarity(df_vectors))
+    similarity_matrix.index = df_vectors.index.astype(str)
+    similarity_matrix.index.name = 'id'
+    similarity_matrix.columns = df_vectors.index.astype(str)
+    recommendations=pd.DataFrame(columns=similarity_matrix.columns)
+    for i in l_prod:
+        #a=df_ll[df_ll['produto_f']==i].index[0]
+        item_id=df_ll[df_ll['produto_f']==i].index[0]
+        #Gerando recomendações
+        target_item_similarities = similarity_matrix.loc[item_id]
+        id_similar_items = (
+            target_item_similarities
+            .sort_values(ascending=False)
+            .reset_index()
+            .rename({'index': 'id', item_id: 'score'}, axis=1)
+        )
+        r=id_similar_items.merge(df_ll[['produto_f']],left_on='id',right_on='id',how='inner').sort_values(by='score', ascending=False)
+        if len(l_prod)>1:
+            recommendations=pd.concat([recommendations,r[1:2]])
+        else:
+            recommendations=pd.concat([recommendations,r[1:5]])
+
+    return recommendations.head()         
+
+def rp_cv(df:pd.DataFrame,l_prod:list)-> pd.DataFrame:
+    #preparando o dataframe para aplicação do algoritimo:
+    dflg=df.reset_index()
+    dflg['produto_full']=dflg['categoria']+" "+dflg['tipo_categoria']+" "+dflg['produto']+" "+dflg['prodcomplemento']
+    recommendations=pd.DataFrame(columns=['item_id', 'score'])
+
+    #criando o grafo:
+    n_users = dflg['cliente_nome'].unique()
+    n_items = dflg['produto_f'].unique()
+    G = nx.Graph()
+    G.add_nodes_from(n_items, node_type='item')
+    G.add_nodes_from(n_users, node_type='user')
+    G.add_edges_from(dflg[['cliente_nome','produto_f']].values)
+    recommendations=pd.DataFrame(columns=['item_id', 'score'])
+    for i in l_prod:
+        item_id=i
+        #Encontrando os itens vizinhos consumidos:
+        neighbors = G.neighbors(item_id)
+        neighbor_consumed_items = []
+        for user_id in neighbors:
+            user_consumed_items = G.neighbors(user_id)
+            neighbor_consumed_items += list(user_consumed_items)
+
+        #Contabilizando os items mais consumidos para criar o score da recomendação:
+        consumed_items_count = Counter(neighbor_consumed_items)
+
+        # Validando tipo do nó
+        node_type = nx.get_node_attributes(G, 'node_type')[item_id]
+        if node_type != 'item':
+            raise ValueError('Node is not of item type.')
+
+        # Contabilizando itens consumidos pelos vizinhos
+        consumed_items_count = Counter(neighbor_consumed_items)
+
+        # Criando dataframe
+        df_neighbors= pd.DataFrame(zip(consumed_items_count.keys(), consumed_items_count.values()))
+        df_neighbors.columns = ['item_id', 'score']
+        df_neighbors = df_neighbors.sort_values(by='score', ascending=False)
+        
+        if len(l_prod)>1:
+            recommendations=pd.concat([recommendations,df_neighbors[1:2]])
+        else:
+            recommendations=pd.concat([recommendations,df_neighbors[1:6]])
+
+    return recommendations
+
+def r_np(df_loja_recnp,l_prod): 
+    if len(l_prod)==0:
+        placeholder1 = st.empty() 
+    else:
+        tab1, tab2, tab3 = st.tabs(["Apriori", "Top N","Content Based"])
+        with tab1:          
+            rec_np=rnp_apr(df_loja_recnp,l_prod)
+            placeholder1 = st.empty()
+            placeholder1.text("Quem comprou estes produtos também comprou:")
+            if rec_np.shape[0]>0:
+                with placeholder1.container():
+                    if len(l_prod)>1:
+                        st.write("Quem comprou estes produtos também comprou:")
+                        for i in rec_np.consequents:
+                            st.write(i)
+                    else:
+                        st.write("Quem comprou este produto também comprou:")
+                        for i in rec_np.consequents:
+                            st.write(i)  
+            else:
+                with placeholder1.container():
+                        st.write("Sem proposições para este item")
+        with tab2:
+            rec_np=rnp_top_n(df_loja_recnp,n=5,l_prod=l_prod)
+            placeholder1 = st.empty()
+            placeholder1.text("Adicione ao carrinho os produtos mais vendidos:")
+            with placeholder1.container():
+                    st.write("Adicione ao carrinho os produtos mais vendidos:")
+                    for i in rec_np.produto_f:
+                        st.write(i)
+        with tab3:
+            rec_np=rnp_cb(df_loja_recnp,l_prod)
+            placeholder1 = st.empty()
+            placeholder1.text("Quem comprou estes produtos também comprou:")
+            with placeholder1.container():
+                    if len(l_prod)>1:
+                        st.write("Quem comprou estes produtos também comprou:")
+                        for i in rec_np.produto_f:
+                            st.write(i)
+                    else:
+                        st.write("Quem comprou este produto também comprou:")
+                        for i in rec_np.produto_f:
+                            st.write(i)            
+
+def r_p(df_loja_recnp,l_prod):
+    if len(l_prod)==0:
+        placeholder2 = st.empty() 
+    else:
+        tab4, tab5 = st.tabs(["Co-visitation", 'Nearest Neighbors'])
+        with tab4:          
+            rec_p=rp_cv(df_loja_recnp,l_prod)
+            placeholder2 = st.empty()
+            placeholder2.text("Quem comprou estes produtos também comprou:")
+            with placeholder2.container():
+                    if len(l_prod)>1:
+                        st.write("Quem comprou estes produtos também comprou:")
+                        for i in rec_p.item_id:
+                            st.write(i)
+                    else:
+                        st.write("Quem comprou este produto também comprou:")
+                        for i in rec_p.item_id:
+                            st.write(i)
+
 
 
 if len(st.session_state.l_prod)==0:
@@ -17,7 +242,7 @@ if len(st.session_state.l_prod)==0:
 else:
         state=False
 
-f_carrinho()
+df_loja_recnp=f_carrinho()
 
 if st.button('Del item',disabled=state):
     if len(st.session_state.l_prod)==1:
@@ -38,3 +263,6 @@ if st.button('Del carrinho',disabled=state):
         st.session_state.l_prod=[]
         st.write (f"Carrinho limpo.") 
         placeholder.empty()
+
+r_np(df_loja_recnp,st.session_state.l_prod)
+r_p(df_loja_recnp,st.session_state.l_prod)
