cb62ee4c7fd7667769223c5fac908ade2c8cb878
vpbatista85 <vitor_pbatista@outlook.com>
2023-02-02 18:10:47 -0300

    minor fix on cart.py

--- pages/3_cart.py
+++ pages/3_cart.py
@@ -20,7 +20,7 @@ utils.f_carrinho()
 
 if st.button('Del item',disabled=state):
     if len(st.session_state.l_prod)==1:
-        st.write (f"{st.session_state.l_prod}, removido do carrinho.")
+        st.write (f"{st.session_state.l_prod.values}, removido do carrinho.")
         st.session_state.l_prod=[]
         state=True
         #placeholder.empty()
