487cf5a8960a9d23c375784351556a697d8d5393
vpbatista85 <vitor_pbatista@outlook.com>
2023-02-04 12:53:26 -0300

    minor fix on cart file and utlis for time functions

--- pages/3_cart.py
+++ pages/3_cart.py
@@ -22,12 +22,12 @@ with st.sidebar:
     horario=st.checkbox('Horario atual', value=True, key=None, help='Marque para usar o horário local', on_change=None, args=None, kwargs=None, disabled=False)
     if horario:
        st.write("Relógio:",datetime.strptime(str(datetime.now()-timedelta(hours=3)),"%Y-%m-%d %H:%M:%S.%f").strftime("%H:%M"))
-       hora=st.slider('Selecione o horário',min_value=time.min,max_value=time.max,value=(datetime.now()-timedelta(hours=3)),step=timedelta(minutes=1),format="HH:MM",disabled=True)
+       hora=st.slider('Selecione o horário',min_value=time.min,max_value=time.max,value=(datetime.now()),step=timedelta(minutes=1),format="HH:MM",disabled=True)
        #hora=st.slider('Selecione o horário', min_value=0, max_value=24,value=(datetime.now()-timedelta(hours=3)), step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=True)
     else:
        #hora=st.slider('Selecione o horário', min_value=0, max_value=24, value=None, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
         st.write("Relógio:",datetime.strptime(str(datetime.now()-timedelta(hours=3)),"%Y-%m-%d %H:%M:%S.%f").strftime("%H:%M"))
-        hora=st.slider('Selecione o horário',min_value=time.min,max_value=time.max,value=(datetime.now()-timedelta(hours=3)),step=timedelta(minutes=5),format="HH:MM",disabled=False)
+        hora=st.slider('Selecione o horário',min_value=time.min,max_value=time.max,value=(datetime.now()),step=timedelta(minutes=5),format="HH:MM",disabled=False)
 
 utils.f_carrinho()
 
