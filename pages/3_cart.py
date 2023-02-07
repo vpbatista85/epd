819ce8b5a13ce0aaae7061f0575fe0d497bef763
vpbatista85 <vitor_pbatista@outlook.com>
2023-02-07 20:19:06 -0300

    changing buttons del item and del cart placing them side by side

--- pages/3_cart.py
+++ pages/3_cart.py
@@ -25,26 +25,33 @@ else:
 
 utils.f_carrinho()
 
-if st.button('Del item',disabled=state):
-    if len(st.session_state.l_prod)==1:
-        st.write (f"{st.session_state.l_prod.values}, removido do carrinho.")
-        st.session_state.l_prod=[]
-        state=True
-        #placeholder.empty()
-    else:    
-        st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
-        st.session_state.l_prod.pop()
-        #placeholder.empty()  
-        placeholder.text("Carrinho:")
-        with placeholder.container():
-            st.write('Carrinho:')
-            for i in st.session_state.l_prod[0:-2]:
-                st.write(i)
+col1, col2= st.columns([1,1])
 
-if st.button('Del carrinho',disabled=state):
+with col1:
+    if st.button('Del item',disabled=state):
+        if len(st.session_state.l_prod)==1:
+            st.write (f"{st.session_state.l_prod.values}, removido do carrinho.")
+            st.session_state.l_prod=[]
+            state=True
+            #placeholder.empty()
+        else:    
+            st.write (f"{st.session_state.l_prod[-1]}, removido do carrinho.")
+            st.session_state.l_prod.pop()
+            #placeholder.empty()  
+            placeholder.text("Carrinho:")
+            with placeholder.container():
+                st.write('Carrinho:')
+                for i in st.session_state.l_prod[0:-2]:
+                    st.write(i)
+
+with col2:
+    if st.button('Del carrinho',disabled=state):
         st.session_state.l_prod=[]
         st.write (f"Carrinho limpo.") 
         #placeholder.empty()
+
+
+
 
 df_loja_rec=st.session_state.df_lrecnp
 # df_loja_rec['dth_hora'] = df_loja_rec['dth_agendamento'].apply(utils.extract_hour)
